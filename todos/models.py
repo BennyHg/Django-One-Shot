55a7a88e98536428905fb360323c996fe70db533
benny.huang <bhuang997@gmail.com>
2022-06-14 13:02:49 -0700

    Feature 7 Complete

--- todos/models.py
+++ todos/models.py
@@ -7,6 +7,7 @@ class TodoList(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
     
+
     def __str__(self):
         return self.name
 
